d76f835643bdb3343dc3de65276504bc1645e9ff
Silvia Stoyanova <stoianova.silvia@yahoo.com>
2018-11-13 12:33:52 +0200

    add todo-item component

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './components/input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputButtonUnitComponent
+    InputButtonUnitComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
